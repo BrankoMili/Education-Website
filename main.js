a78692f5405346aab02a16ee4b5896654df30fd7
BrankoMili <105740275+BrankoMili@users.noreply.github.com>
2022-05-27 15:13:42 +0200

    form height

--- main.js
+++ main.js
@@ -1,30 +1,26 @@
 // Change NAVBAR styles on scroll
 
-window.addEventListener("scroll", () => {
-    document.querySelector('nav').classList.toggle
-    ('window-scroll', window.scrollY > 0)
-}    
-)
+window.addEventListener('scroll', () => {
+	document.querySelector('nav').classList.toggle('window-scroll', window.scrollY > 0);
+});
 
-
-// show/hide faq answer 
+// show/hide faq answer
 
 const faqs = document.querySelectorAll('.frequently');
 
-faqs.forEach(frequently => {
-    frequently.addEventListener('click', () => {
-        frequently.classList.toggle('open');
+faqs.forEach((frequently) => {
+	frequently.addEventListener('click', () => {
+		frequently.classList.toggle('open');
 
-        // change icon
-        const icon = frequently.querySelector('.frequently__icon i');
-        if(icon.className === 'uil uil-plus') {
-            icon.className = 'uil uil-minus'
-        } else {
-            icon.className = 'uil uil-plus'
-        }
-    })
-})
-
+		// change icon
+		const icon = frequently.querySelector('.frequently__icon i');
+		if (icon.className === 'uil uil-plus') {
+			icon.className = 'uil uil-minus';
+		} else {
+			icon.className = 'uil uil-plus';
+		}
+	});
+});
 
 //show/hide nav menu
 const menu = document.querySelector('.nav__menu');
@@ -32,16 +28,31 @@ const menuBtn = document.querySelector('#open-menu-btn');
 const closeBtn = document.querySelector('#close-menu-btn');
 
 menuBtn.addEventListener('click', () => {
-    menu.style.display = "flex";
-    closeBtn.style.display = "inline-block";
-    menuBtn.style.display = "none";
-})
+	menu.style.display = 'flex';
+	closeBtn.style.display = 'inline-block';
+	menuBtn.style.display = 'none';
+});
 
 //close nav menu
 const closeNav = () => {
-    menu.style.display = "none";
-    closeBtn.style.display = "none";
-    menuBtn.style.display = "inline-block";
-}
+	menu.style.display = 'none';
+	closeBtn.style.display = 'none';
+	menuBtn.style.display = 'inline-block';
+};
 
 closeBtn.addEventListener('click', closeNav);
+
+const ivisiblebtn = () => {
+	if (window.innerWidth >= 1024) {
+		menu.style.display = 'flex';
+		menu.style.gap = 4;
+		closeBtn.style.display = 'none';
+		menuBtn.style.display = 'none';
+	} else {
+		menuBtn.style.display = 'flex';
+		menu.style.display = 'none';
+	}
+};
+
+window.addEventListener('resize', ivisiblebtn);
+
